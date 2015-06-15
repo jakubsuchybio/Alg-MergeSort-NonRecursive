dde3d872dec2762e59e7bf9b68db2d40d385e297
Jakub Suchy <jakubsuchy@outlook.com>
2015-06-16 00:47:56 +0200

    Initial commit.

---
+++ MergeSort/MergeSort/MergeSort.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    public class MergeSort
+    {
+        /// <summary>
+        /// Non-recursive implementation of MergeSort
+        /// </summary>
+        /// <param name="array">Input array to get sorted.</param>
+        public static void Sort( ref int[] array ) {
+            if( array.Length < 2 )
+                return;
+
+            int step = 1;
+            int startL, startR;
+            while( step < array.Length ) {
+                startL = 0;
+                startR = step;
+                while( startR + step <= array.Length ) {
+                    Merge( ref array, startL, startL + step, startR, startR + step );
+                    startL = startR + step;
+                    startR = startL + step;
+                }
+                if( startR < array.Length )
+                    Merge( ref array, startL, startL + step, startR, array.Length );
+                step *= 2;
+            }
+        }
+
+        private static void Merge( ref int[] array, int startL, int stopL, int startR, int stopR ) {
+            int[] left = new int[ stopL - startL + 1 ];
+            int[] right = new int[ stopR - startR + 1 ];
+
+            Array.Copy( array, startR, right, 0, right.Length - 1 );
+            Array.Copy( array, startL, left, 0, left.Length - 1 );
+
+            right[ right.Length - 1 ] = Int32.MaxValue;
+            left[ left.Length - 1 ] = Int32.MaxValue;
+
+            for( int k = startL, m = 0, n = 0; k < stopR; ++k ) {
+                if( left[ m ] <= right[ n ] ) {
+                    array[ k ] = left[ m ];
+                    m++;
+                }
+                else {
+                    array[ k ] = right[ n ];
+                    n++;
+                }
+            }
+        }
+    }
+}
---
+++ MergeSort/MergeSortTests/MergeSortTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MergeSort;
+
+namespace MergeSortTests
+{
+    [TestClass]
+    public class MergeSortTests
+    {
+
+        [TestMethod]
+        public void SortOne() {
+            int[] input = new int[] { 5 };
+
+            MergeSort.MergeSort.Sort( ref input );
+
+            CollectionAssert.AreEqual( new int[] { 5 }, input );
+        }
+
+        [TestMethod]
+        public void SortTwo() {
+            int[] input = new int[] { 5, 4 };
+            int[] expected = new int[] { 4, 5 };
+
+            MergeSort.MergeSort.Sort( ref input );
+
+            CollectionAssert.AreEqual( expected, input );
+        }
+
+        [TestMethod]
+        public void SortTwo2() {
+            int[] input = new int[] { 4, 5 };
+            int[] expected = new int[] { 4, 5 };
+
+            MergeSort.MergeSort.Sort( ref input );
+
+            CollectionAssert.AreEqual( expected, input );
+        }
+
+        [TestMethod]
+        public void SortFive() {
+            int[] input = new int[] { 5, 4, 3, 2, 1 };
+            int[] expected = new int[] { 1, 2, 3, 4, 5 };
+
+            MergeSort.MergeSort.Sort( ref input );
+
+            CollectionAssert.AreEqual( expected, input );
+        }
+    }
+}
